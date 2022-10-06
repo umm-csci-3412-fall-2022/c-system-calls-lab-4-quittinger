83f7c7ee7c525ecbbb824b47eefc3f47e309e5af
Conner <hetti031@morris.umn.edu>
2022-10-06 16:00:17 -0500

    Add the base code for file_disemvowel.c

---
+++ file_disemvowel/disemvowel.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define BUF_SIZE 1024
+
+bool is_vowel(char c) {
+    /*
+     * Returns true if c is a vowel (upper or lower case), and
+     * false otherwise.
+     */
+}
+
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+    /*
+     * Copy all the non-vowels from in_buf to out_buf.
+     * num_chars indicates how many characters are in in_buf,
+     * and this function should return the number of non-vowels that
+     * that were copied over.
+     */
+}
+
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+    /*
+     * Copy all the non-vowels from inputFile to outputFile.
+     * Create input and output buffers, and use fread() to repeatedly read
+     * in a buffer of data, copy the non-vowels to the output buffer, and
+     * use fwrite to write that out.
+     */
+}
+
+int main(int argc, char *argv[]) {
+    // This sets these to `stdin` and `stdout` by default.
+    // You then need to set them to user specified files when the user
+    // provides files names as command line arguments.
+    FILE *inputFile = stdin;
+    FILE *outputFile = stdout;
+
+    // Code that processes the command line arguments
+    // and sets up inputFile and outputFile.
+
+    disemvowel(inputFile, outputFile);
+
+    return 0;
+}
