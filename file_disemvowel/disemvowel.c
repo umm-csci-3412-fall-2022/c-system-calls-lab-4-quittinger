47654a9e398aaedc167e5a64fbc45998c385ee8c
Conner <hetti031@morris.umn.edu>
2022-10-11 16:02:20 -0500

    We started writing the .c file
    we believe we've gotten it to take in a file and read it and handle the buffers

--- file_disemvowel/disemvowel.c
+++ file_disemvowel/disemvowel.c
@@ -7,11 +7,19 @@
 
 #define BUF_SIZE 1024
 
+
 bool is_vowel(char c) {
     /*
      * Returns true if c is a vowel (upper or lower case), and
      * false otherwise.
      */
+        char *vowels = (char*)calloc(11, sizeof(char));
+  strcpy(vowels, "aeiouAEIOU\0");
+                if(strchr(vowels, c) != NULL){
+                        return true;
+                }
+        }
+free(vowels);
 }
 
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
@@ -30,14 +38,40 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
      * in a buffer of data, copy the non-vowels to the output buffer, and
      * use fwrite to write that out.
      */
+      char out_buf[BUF_SIZE];
+      char in_buf[BUF_SIZE];
+      int nchar;
+      of = fopen(outputFile, "w+");
+      fp = fopen(inputFile, "w+");
+      nchar = fread(in_buf,BUF_SIZE,1,fp);
+      while(nchar != 0){
+        int output = copy_non_vowels(nchar, in_buf, out_buf);
+        fwrite(out_buf,output,1,of)
+        fflush(inputFile);
+        fflush(outputFile);
+        fseek(fp,0,SEEK_CUR);
+        nchar = fread(in_buf,BUF_SIZE,1,fp);
+      }
+
+
 }
 
 int main(int argc, char *argv[]) {
     // This sets these to `stdin` and `stdout` by default.
     // You then need to set them to user specified files when the user
     // provides files names as command line arguments.
+
+
+
+
     FILE *inputFile = stdin;
     FILE *outputFile = stdout;
+
+    if(argc == 2){
+            inputFile = argv[1];
+    }
+    if(argc == 3){
+            inputFile = argv[1];
 
     // Code that processes the command line arguments
     // and sets up inputFile and outputFile.
