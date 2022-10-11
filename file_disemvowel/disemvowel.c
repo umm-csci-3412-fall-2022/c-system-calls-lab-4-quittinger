a8e117ccce9464a62dffde0d362d921b3e4997d7
Conner <hetti031@morris.umn.edu>
2022-10-11 16:21:07 -0500

    Compiled our code into frogs and fixed minor issues
    We still have a lot of issues to fix, but this is progress (according to max)

--- file_disemvowel/disemvowel.c
+++ file_disemvowel/disemvowel.c
@@ -16,10 +16,11 @@ bool is_vowel(char c) {
         char *vowels = (char*)calloc(11, sizeof(char));
   strcpy(vowels, "aeiouAEIOU\0");
                 if(strchr(vowels, c) != NULL){
+			free(vowels);
                         return true;
                 }
-        }
-free(vowels);
+		free(vowels);
+		return false;	 
 }
 
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
@@ -29,10 +30,23 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
      * and this function should return the number of non-vowels that
      * that were copied over.
      */
+
+  int i,x=0;
+  for (i=0; i<num_chars; ++i) {
+	if(!is_vowel(in_buf[i])){
+			out_buf[x] = in_buf[i];
+			++x;
+	}
+  }
+	return x;
 }
 
+
 void disemvowel(FILE* inputFile, FILE* outputFile) {
-    /*
+   FILE *fp;
+   FILE *of;
+
+      	/*
      * Copy all the non-vowels from inputFile to outputFile.
      * Create input and output buffers, and use fread() to repeatedly read
      * in a buffer of data, copy the non-vowels to the output buffer, and
@@ -46,7 +60,7 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
       nchar = fread(in_buf,BUF_SIZE,1,fp);
       while(nchar != 0){
         int output = copy_non_vowels(nchar, in_buf, out_buf);
-        fwrite(out_buf,output,1,of)
+        fwrite(out_buf,output,1,of);
         fflush(inputFile);
         fflush(outputFile);
         fseek(fp,0,SEEK_CUR);
@@ -72,7 +86,7 @@ int main(int argc, char *argv[]) {
     }
     if(argc == 3){
             inputFile = argv[1];
-
+    }
     // Code that processes the command line arguments
     // and sets up inputFile and outputFile.
 
